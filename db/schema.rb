26b4fd988ef7404d0fca0521a9ef20ac8fd83513
ankin <akinash@gmail.com>
2014-11-01 15:20:13 +0100

    devise styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030103951) do
+ActiveRecord::Schema.define(version: 20141101122034) do
 
   create_table "attachments", force: true do |t|
     t.text     "text"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20141030103951) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
